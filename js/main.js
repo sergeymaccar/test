d5fbb8a10c6d8287f2e2c80a45130ed8f1e90803
sergeymaccar <sergeymaccar@gmail.com>
2020-09-01 16:01:28 +0300

    new section FAQ

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+// FAQ section
+
+// Get the container element
+var btnContainer = document.getElementById("nav-tabs");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("tabs-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-tab");
+    current[0].className = current[0].className.replace(" active-tab", "");
+    this.className += " active-tab";
+  });
+}
